1f3ae6551e1e69e8fbdc9cb0ebd0069750299f3f
sneha-patel18 <snehapatel182007@gmail.com>
2024-07-20 12:56:54 +0530

    20/07/2024 PR-1

---
+++ Ch-1/1_2/2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   int x,y,ans;
+   clrscr();
+
+   printf("(x+y)2 \t:");
+   printf("enter a number to apply in an formula:");
+   scanf("%d",x);
+   printf("enter second number);
+   scanf("%d",y);
+   ans =(x*x)+(2*x*y)+(y*y);
+   ans("")
+
+   getch();
+}
